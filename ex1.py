c3165b76cbae235618972bde1d56890a09b8e184
Daniel Ziebart <daniel.ziebert@dci-student.org>
2023-09-14 16:09:07 +0200

    New Tasks added

---
+++ ex1.py
@@ -0,0 +1,107 @@
+# """
+# This is a multiline string
+# it is used by docstring
+# We will cover it later in depth
+# """
+
+
+
+# print("Hello World!")
+# print("Das ist wohl alles schaffbar")
+# print("Actually, there are more books, including 'Python for programmers by Deite'")
+# print("Hello", "World", "Daniel", sep="*")
+# print("Hello", "World", "Daniel", sep="\n")
+# x = "Bananas, Ananas, Apples"
+# print(x)
+
+
+# print(y)
+# # Here i can leave a comment when working in 5 weeks.
+
+# print(x,y)
+# y = ["First", "Second"]
+# my_num = 256
+# my_num ="256"
+# print(my_num)
+# print(my_num+my_num)
+# print(type(y))
+# # print(x+y) Mixint diferent Types of Date can be problem
+
+# x = "This ist my first variable using a simple print()."
+# y = 53
+# print("Y value is equal to " + str(y))
+# print(type(y))
+# print(str(y)+str(y))
+
+# print("Lets try to convert in the opposite direction: string to an number")
+# z = "54"
+# z = int(z)
+# print(type(z))
+#! Warning!
+#? Should we use this 
+
+'''This is a multiline
+comment'''
+# myvar = "John"
+# my_var = "John"
+# My_var = "John"
+# MY_var = "Dima"
+
+# print(my_var)
+#! print(MY_var)
+# print(My_var)
+
+f = 123.96545216
+# print(f)
+# print(type(f)) # getting type of number
+# print(int(f))
+# print(float(f))
+# print(isinstance(f, float))
+
+# person = "Daniel", "Ziebart", "Talbotstr.", "2", "52068", "Aachen"
+# print(person)
+
+#Boolean data type
+# b = True
+# print(b)
+# print(type(b))
+
+# Imaginary numbers
+# i = 4 + 5j
+# print(i)
+# print(type(i))
+
+
+x = 123
+y = 43.23
+c = (4-1j)
+d = "How are you?"
+v = True
+
+# #Task 1
+# print(int(x))
+# print(float(y))
+# print(complex(c))
+# print(str(d))
+# print(bool(v))
+
+# #Task 2
+# print(x, type(x), sep=" is type of")
+# print(y, type(y), sep=" is type of")
+# print(c, type(c), sep=" is type of")
+# print(d, type(d), sep=" is type of")
+# print(v, type(v), sep=" is type of")
+
+# #Task 3
+# print(isinstance(x, int))
+# print(isinstance(y, complex))
+# print(isinstance(c, complex))
+# print(isinstance(d, str))
+# print(isinstance(v, complex))
+
+print("Is", x," an instance of int?:", isinstance(x, int))
+print("Is", y," an instance of bool?:", isinstance(y, int))
+print("Is", c," an instance of complex?:", isinstance(c, complex))
+print("Is", d," an instance of bool?:", isinstance(d, bool))
+print("Is", v," an instance of float?:", isinstance(v, int))
+
---
+++ hello.py
@@ -0,0 +1,38 @@
+import json
+import requests
+
+print("myobj.js")
+
+const myObj = {
+    "first_name": "Daniel",
+    "last_name": "Ziebart",
+    "birthplace": "Guliston in Tadschikistan",
+    "birth_date": "27.01.1982",
+    "age": "41",
+    "gender": "male",
+    "nationality": "german",
+    "is_married": true,
+    "has_children": true, 
+    "hobbies": ["read", "hiking", "do something with my son"],
+    "favorite_tv_shows": "none",
+    "pets": "two cats"
+};
+
+const address = {
+    "street": "Talbotstr.",
+    "zip_code": "52068",
+    "house_number": "2",
+    "district_name": "Mitte",
+    "city": "Aachen",
+    "country": "Germany"
+};
+
+const car = {
+    "model_name": "X5",
+    "type": "SUV",
+    "brand_name": "BMW",
+    "year": "2023"
+};
+
+>>> 1 + 2
+
